bf62154fffe95b86ca9454ccb4db0797da9742e2
Neema Philippe <neemaphilippe@pursuit.org>
2020-12-03 00:11:59 -0500

    Fixed stackview constraints in NewTripEntryVC

--- TravelPP/TravelVCs/NewTripEntryVC.swift
+++ TravelPP/TravelVCs/NewTripEntryVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import UIKit
+
 class NewTripEntryVC: UIViewController {
     
     //MARK: UI Objects
@@ -36,19 +38,14 @@ class NewTripEntryVC: UIViewController {
         let label = UILabel()
         label.font = UIFont(name: "Times New Roman", size: 20)
         label.text = "Where to next?"
-//        label.textAlignment = .left
         label.numberOfLines = 0
-//        label.sizeToFit()
         return label
     }()
-    
     
     lazy var newLocationText : UITextField = {
         let tf = UITextField()
         tf.font = UIFont(name: "Times New Roman", size: 14)
-        tf.backgroundColor = .white
         tf.borderStyle = .bezel
-        tf.autocorrectionType = .no
         return tf
     }()
     
@@ -65,7 +62,6 @@ class NewTripEntryVC: UIViewController {
     lazy var newDatePicker: UIDatePicker = {
         let date = UIDatePicker()
         date.timeZone = NSTimeZone.local
-        date.backgroundColor = UIColor.green
         return date
     }()
     
@@ -75,9 +71,8 @@ class NewTripEntryVC: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont(name: "Times New Roman", size: 16)
         button.backgroundColor = #colorLiteral(red: 0.599193275, green: 0.7987571359, blue: 0.9307624698, alpha: 1)
-        button.layer.cornerRadius = 5
-        button.isEnabled = false
-        button.addTarget(self, action: #selector(submitButtonPressed), for: .touchUpInside)
+        button.layer.cornerRadius = 25
+        button.addTarget(self, action: #selector(handleSubmitButtonPressed), for: .touchUpInside)
         return button
     }()
     
@@ -91,41 +86,50 @@ class NewTripEntryVC: UIViewController {
         self.contentStackView.addArrangedSubview(newLocationText)
         self.contentStackView.addArrangedSubview(newDateLabel)
         self.contentStackView.addArrangedSubview(newDatePicker)
-        self.view.addSubview(submitButton)
+        self.view.addSubview(self.submitButton)
     }
     
     func constrainWelcomeLabel(){
         welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             welcomeLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-//            welcomeLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             welcomeLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             welcomeLabel.heightAnchor.constraint(equalToConstant: 50),
             welcomeLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor)
         ])
     }
     
-    func constrainNewLocationLabel(){
-        newlocationLabel.translatesAutoresizingMaskIntoConstraints = false
+    func constrainStackView(){
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            newlocationLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 10),
-            newlocationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-//            newlocationLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            newlocationLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            contentStackView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 30),
+            contentStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
+            contentStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
         ])
     }
     
-    func constrainNewLocationText(){
-        newLocationText.translatesAutoresizingMaskIntoConstraints = false
+    func constrainSubmitButton() {
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            newLocationText.widthAnchor.constraint(equalToConstant: 150),
-            newLocationText.heightAnchor.constraint(equalToConstant: 40),
-            newLocationText.centerYAnchor.constraint(equalTo: newlocationLabel.centerYAnchor),
-            newLocationText.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30)
+            submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            submitButton.topAnchor.constraint(equalTo: self.contentStackView.bottomAnchor, constant: 30),
+            submitButton.widthAnchor.constraint(equalToConstant: 350),
+            submitButton.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    @objc private func handleSubmitButtonPressed() {
+        guard self.newLocationText.text != "" else {
+            let alertVC = UIAlertController(title: "YOU FUCKED UP BITCH", message: nil, preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "sry papi", style: .default, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+            return
+        }
         
+        //TODO: -- Make persistence happen
+        //            let newTravelInfo = TravelInfo(location: self.newLocationText.text, departureDate: self.newDatePicker.date, currentDate: Date())
+        //            travelPersistenceManager.shared.save(newTravelInfo)
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -133,49 +137,7 @@ class NewTripEntryVC: UIViewController {
         addViews()
         constrainWelcomeLabel()
         constrainStackView()
-//        constrainNewLocationLabel()
-//        constrainNewLocationText()
-//        constrainDatePicker()
-       
+        constrainSubmitButton()
+        newLocationText.widthAnchor.constraint(equalToConstant: 240).isActive = true
     }
-//
-//    func constrainNewDateLabel(){
-//        newDateLabel.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            newDateLabel.topAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutYAxisAnchor>#>, constant: <#T##CGFloat#>)
-//        ])
-//
-//
-//
-//    }
-    
-    @objc func submitButtonPressed(){
-        if self.newlocationLabel.text != "" {
-            print("It's getting saved!")
-        }
-    }
-    
-    func constrainDatePicker(){
-        newDatePicker.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-//            newDatePicker.leadingAnchor.constraint(equalTo: newLocationText.leadingAnchor),
-            newDatePicker.trailingAnchor.constraint(equalTo: newLocationText.trailingAnchor),
-            newDatePicker.topAnchor.constraint(equalTo: newLocationText.bottomAnchor, constant: 10),
-            newDatePicker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30)
-//            newDatePicker.widthAnchor.constraint(equalTo: newLocationText.widthAnchor)
-        
-        ])
-    }
-    
-    func constrainStackView(){
-        contentStackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            contentStackView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor),
-            contentStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            contentStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
-            contentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        
-        ])
-    }
-
 }
