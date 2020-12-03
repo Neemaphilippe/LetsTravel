7e617eb5ee3f8ea7698c0d9f0d486b79d8672a0d
Neema Philippe <neemaphilippe@pursuit.org>
2020-12-02 20:39:37 -0500

    Added more citites to traveljson in TravelInfoModel

--- TravelPP/Model/TravelInfoModel.swift
+++ TravelPP/Model/TravelInfoModel.swift
@@ -12,18 +12,30 @@ let traveljson = """
 [
   {
     "location": "New York",
-"departureDate": "10/20/10",
-"currentDate": "10/21/10"
-  }
+"departureDate": "12/20/20",
+"currentDate": "12/03/20"
+  }, {
+ "location": "Colorado",
+"departureDate": "01/20/21",
+"currentDate": "12/03/20"
+}, {
+ "location": "Paris",
+"departureDate": "06/08/21",
+"currentDate": "12/03/10"
+}, {
+ "location": "Tokyo",
+"departureDate": "10/20/21",
+"currentDate": "12/03/20"
+}
 ]
 """.data(using: .utf8)!
 
 
 struct TravelInfo: Decodable {
- let location: String
- let departureDate: String
- let currentDate: String
+    let location: String
+    let departureDate: String
+    let currentDate: String
     
-   
+    
 }
 
